1ab55bbd1a29aae4b0b7a1931e1b6de41ac6d02b
rk11machaba <68669221+rk11machaba@users.noreply.github.com>
2023-10-29 19:36:58 +0200

    Update Student Management System.cpp

--- Student Management System.cpp
+++ Student Management System.cpp
@@ -5,38 +5,58 @@
 using namespace std;
 
 class Student {
-//	data members
+	//	data members
 private:
 	string studNum, firstName, lastName;
 
-//	member functions
+	//	member functions
 public:
 	Student(); //	default constructor
-	void PromptUser();
-	void DisplayStudent();
-	void CreateStudent();	//	create a file and store student data
+
+	//setters and getters
+	void setStudent(string, string, string);
+	string getStudNum() { return studNum; }
+	string getFirstName() { return firstName; }
+	string getLastName() { return lastName; }
 };
 
 Student::Student() {
-	cout << "***Student Management System***" << endl;
+	studNum = "";
+	firstName = "";
+	lastName = "";
 }
 
-void Student::PromptUser() {
-	cout << "Student Number: ";
-	cin >> studNum;
-	cout << "First name: ";
-	cin >> firstName;
-	cout << "Lastname: ";
-	cin >> lastName;
+void Student::setStudent(string sn, string fn, string ln) {
+	studNum = sn;
+	firstName = fn;
+	lastName = ln;
 }
 
-void Student::DisplayStudent() {
-	cout << "Student Number: " << studNum << endl;
-	cout << "Name: " << lastName << " " << firstName << endl;
+
+void PromptUser(Student *St,int sz) {
+
+	string sn, fn, ln;
+	for (int i = 0; i < sz;i++) {
+		cout << "Student Number: ";
+		cin >> sn;
+		cout << "First name: ";
+		cin >> fn;
+		cout << "Lastname: ";
+		cin >> ln;
+		St[i].setStudent(sn, fn, ln);
+	}
 }
 
-void Student::CreateStudent() {
-	ofstream outFile(studNum + ".txt"); // filename
+void DisplayStudents(Student *St,int sz) {
+	cout << "***Students***" << endl;
+	for (int i = 0; i < sz; i++) {
+		cout << i + 1 << ": " << St[i].getStudNum() << " " << St[i].getFirstName() << " " << St[i].getLastName() << endl;
+	}
+	cout << "**********************" << endl;
+}
+
+void CreateStudents(Student *St,int sz) {
+	ofstream outFile("Students.txt"); // filename
 
 	if (!outFile.is_open()) {
 		// error message
@@ -44,41 +64,49 @@ void Student::CreateStudent() {
 		exit(0);
 	}
 	else {
-		outFile << "Student Number: \t" << studNum << endl;
-		outFile << "First Name: \t" << firstName << endl;
-		outFile << "Last Name: \t" << lastName << endl;
-		outFile << "********************************" << endl;
-		cout << "Student file created Successfully" << endl;
+		for (int i = 0; i < sz; i++) {
+			outFile << i + 1 << ": " << St[i].getStudNum() << "," << St[i].getFirstName() << "," << St[i].getLastName() << endl;
+		}
+		cout << "Students file created Successfully" << endl;
 	}
 }
 
 int main() {
 
-	Student obj; // create object
+	cout << "***Student Management System***" << endl;
 
-	// variables declaration
+	// declare variables
+	Student obj; // create object
 	int option;
+	int numStudents = 0;
 
 	//	Menu
 	cout << "***Menu***" << endl;
-	cout << "Add new Student:\t1" << endl;
-	cout<<"Search for a Student:\t2" << endl;
+	cout << "Add new Students:\t1" << endl;
+	cout << "Search for a Student:\t2" << endl;
 	cout << "Exit the App!:\t0" << endl;
 	cout << "\nOption: ";
 	cin >> option;
 
+	cout << "How many students do you want to enter: ";
+	cin >> numStudents;
+	Student* St = new Student[numStudents];
+
 	// if statement
 	if (option == 0) {
 		exit(1);
-	}else
+	}
+	else
 		if (option == 1) {
+
 			//call member functions
-			obj.PromptUser();
-			obj.CreateStudent();
-		}else
+			PromptUser(St,numStudents);
+			CreateStudents(St,numStudents);
+		}
+		else
 			if (option == 2) {
 				//call member function
-				obj.DisplayStudent();
+				DisplayStudents(St,numStudents);
 			}
 	system("pause");
 }
